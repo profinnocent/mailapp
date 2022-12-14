2b0c38d748de0d16363cdea064b1ce888e98984a
Innocent Unachukwu <innocent.unachukwu@gmail.com>
2022-12-14 23:00:32 +0100

    implemented forget pass functionality

--- includes/forgetpw.php
+++ includes/forgetpw.php
@@ -1,4 +1,5 @@
 <?php
+require_once('connection.php');
 include('functions.php');
 
 if(isset($_POST['submitbtn'])){
@@ -13,27 +14,28 @@ if(isset($_POST['submitbtn'])){
 			$uemail = mysql_prep($connect, trimData($sanitizer['email2']));
 			
 			if(!filter_var($uemail, FILTER_VALIDATE_EMAIL)){
-				$error = urlencode('please enter a correct email format');
+				$error = urlencode("Please enter a valid email format");
 				header('Location: ../public/index.php?error='.$error);
 				return false;
 			}else{
-				$checkemail = "SELECT `password` FROM `users` WHERE `email` = '$uemail'";
+				$checkemail = "SELECT `id` FROM `users` WHERE `email` = '$uemail'";
 				$check_res = mysqli_query($connect, $checkemail);
 				if(mysqli_num_rows($check_res) > 0){
 					$rows = mysqli_fetch_assoc($check_res);
-                    print_r($rows);
+                    // print_r($rows);
 
-					// session_start();
-					// $_SESSION['id'] = $rows['id'];
-					// $_SESSION['email'] = $rows['email'];
-					// if (isset($_SESSION['id'])) {
-					// 	header('location: ../public/dashboard.php');
-					// 	return false;
-					// }else{
-					// 	$error = urlencode('login failed');
-					// 	header('location: ../public/index.php?error='.$error);
-					// 	return false;
-					// }
+                    $pw = base64_encode($rows['id']);
+                    echo $pw;
+
+                    $success = urlencode("Your new pass link $pw has been sent to your mail");
+                    header('Location: ../public/index.php?success='.$success);
+                    return false;
+
+
+                    // mail("oiunachukwu@gmail.com", "reset", "$pw");
+
+					// Send new passowrd link to change link or passcode
+
 					
 				}else{
 					$error = urlencode('user does not exists');
@@ -44,7 +46,7 @@ if(isset($_POST['submitbtn'])){
 
         }else{
 			// $error = urlencode($check);
-            $error = urlencode('check failed');
+            $error = urlencode('Please enter an Email');
 			header('Location: ../public/index.php?error='.$error);
 			return false;
 		}
